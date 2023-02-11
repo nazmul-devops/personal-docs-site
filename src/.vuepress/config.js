e19dc409aeb637d4739c30bea431eae1c9706c39
niswapan <niswapan@gmail.com>
2023-02-11 15:00:17 +0600

    config.js updated

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -5,6 +5,7 @@ module.exports = {
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
   title: "Tech With Nazmul",
+  base: "/nazmul/",
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
